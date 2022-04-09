db83508b9a07b47e7a4db0102868c97c5227ff4d
Breno <bcaua205@gmail.com>
2022-04-08 22:20:46 -0300

    Up

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using CriandoProjeto.Models;
+using System.Linq;
+
+namespace CriandoProjeto.Controllers 
+{
+    public class HomeController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        public IActionResult Usuarios()
+        {
+            return View(Usuario.Listagem);
+        } 
+        
+        [HttpGet]
+        public IActionResult Cadastrar(int? id)
+        {
+            Usuario user = new Usuario();
+            if(id.HasValue && Usuario.Listagem.Any(u => u.Id == id))
+            {
+               user = Usuario.Listagem.Single(u => u.Id == id);
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario user)
+        {
+            Usuario.Salvar(user);
+            return RedirectToAction("Usuarios");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            Usuario user = new Usuario();
+            if(id.HasValue && Usuario.Listagem.Any(u => u.Id == id))
+            {
+               user = Usuario.Listagem.Single(u => u.Id == id);
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(Usuario user)
+        {
+            Usuario.Excluir(user.Id);
+            return RedirectToAction("Usuarios");
+        }
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace CriandoProjeto.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        private static List<Usuario> listagem = new List<Usuario>();
+        public static IQueryable<Usuario> Listagem 
+        {
+            get 
+            {
+                return listagem.AsQueryable();
+            }
+        }
+
+        static Usuario()
+        {
+            Usuario.listagem.Add(new Usuario { Id = 1, Nome = "Fulano", Email = "fulano@email.com"});
+            Usuario.listagem.Add(new Usuario { Id = 2, Nome = "IDk", Email = "fulano@email.com"});
+            Usuario.listagem.Add(new Usuario { Id = 3, Nome = "Beltrano", Email = "fulano@email.com"});
+            Usuario.listagem.Add(new Usuario { Id = 4, Nome = "Liss", Email = "fulano@email.com"});
+            Usuario.listagem.Add(new Usuario { Id = 5, Nome = "Layon", Email = "fulano@email.com"});
+        }
+
+        public static void Salvar(Usuario user)
+        {
+            var usuarioExistente = Usuario.listagem.Find(u => u.Id == user.Id);
+            if(usuarioExistente != null)
+            {
+                usuarioExistente.Nome = user.Nome;
+                usuarioExistente.Email = user.Email;
+            } 
+            else
+            {
+                int maiorId = Usuario.listagem.Max(u => u.Id);
+                user.Id = maiorId + 1;
+                Usuario.listagem.Add(user);
+            }
+        }
+
+        public static void Excluir(int id)
+        {
+            var conta = Usuario.listagem.Find(x => x.Id == id);
+            if(conta != null)
+            {
+                Usuario.listagem.Remove(conta);
+            }
+        } 
+    }
+}
